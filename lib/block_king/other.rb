a3aaeb12d7514c39792a0e2e88b46f0a45c91097
soukouki <soukouki0@yahoo.co.jp>
2019-12-20 16:13:44 +0900

    block_king.rbに残っているコードを新しいファイルに移動

---
+++ lib/block_king/other.rb
@@ -0,0 +1,158 @@
+
+# 絶対ポス
+# absolute position
+AbPos = Struct.new(:x, :y) do
+	def to_s
+		"#{x}, #{y}"
+	end
+	def diff_to_ab_pos(diff_x, diff_y)
+		AbPos.new(x+diff_x, y+diff_y)
+	end
+end
+AbPos::CENTER = AbPos.new(0, 0)
+
+Item = Struct.new(:name) do
+	def to_s
+		name
+	end
+	
+	# どこに置くか悩んでる
+	def self.count_by_items_hash_to_s(hash, join_str: "、", inline_code_count: true)
+		cc = (inline_code_count)? "`" : ""
+		hash.map{|item,count|"#{item}を#{cc}#{count}#{cc}"}.join(join_str)
+	end
+end
+class Block
+	attr_reader :level
+	def initialize()
+		ここは来ないはずです・・
+	end
+	def eql?(o)
+		name.eql?(o.name) && level.eql?(o.level)
+	end
+	def hash
+		name.hash ^ level.hash
+	end
+	def to_s
+		name
+	end
+	def empty?
+		false
+	end
+	def creatable_items
+		GameData::RECIPES.select{|r|r.main_building == self.class}
+	end
+	def map_name
+		name
+	end
+end
+class Nature < Block
+	attr_accessor :remaining_items
+	def initialize(level)
+		@level = level
+		@remaining_items = level * 10
+		@maximum_items = @remaining_items
+	end
+	def turn_items(group)
+		item = get_items_when_turning
+		if item && @remaining_items > 0
+			count = if @remaining_items > @maximum_items/2
+				level*2
+			else
+				(level*2.0 * @remaining_items*1.0 / @maximum_items).ceil
+			end
+			[item, [count, (group.soldier*rand(1.0..2.0) + 15).to_i].min]
+		else
+			[]
+		end
+	end
+	def remaining_items_text
+		case 1.0 * @remaining_items / @maximum_items
+		when 0.9..Float::INFINITY
+			"たくさんあります！"
+		when 0.7..1
+			"まだまだあります！"
+		when 0.5..1
+			"だいぶ減ってきました。"
+		when 0.3..1
+			"残り少なくなりました。移ったほうがいいかもしれません。"
+		when 0.1..1
+			"もう少ししか残っていません。移ったほうがいいかもしれません。"
+		else
+			"殆ど残ってなさそうです。移ったほうがいいでしょう。"
+		end
+	end
+	def get_items_when_turning; nil end
+	def few_remaining_item?
+		1.0 * @remaining_items / @maximum_items <= 0.2
+	end
+end
+class Building < Block
+	attr_reader :builder
+	def initialize(builder, level)
+		@builder = builder
+		@level = level
+	end
+	def == o
+		self == o && o.builder == @builder
+	end
+	def turn_items(group); [] end
+	def get_items_when_turning; nil end
+	def need_items
+		GameData::CAN_BUILD_LIST[self.class]
+	end
+end
+
+def Block.new_type(type_name, &block)
+	Class.new(self) do # ここのselfはNatureとかになる
+		define_method(:name) do
+			type_name
+		end
+		define_singleton_method(:type_name) do
+			type_name
+		end
+		define_singleton_method(:defi) do |key, &defi_block|
+			define_method(key){defi_block.call}
+		end
+		block.call(self)
+	end
+end
+
+Recipe = Struct.new(:main_building, :auxiliary_buildings, :materials_hash, :products_hash, :production_time) do
+	def can_craft?(now_location_building, adjacent_buildings, owns_items)
+		main_building == now_location_building && enough_adjacent_buildings?(adjacent_buildings) && enough_items?(owns_items)
+	end
+	
+	def enough_adjacent_buildings?(adjacent_buildings)
+		auxiliary_buildings.all?{|b|adjacent_buildings.include? b}
+	end
+	def enough_items?(owns_items)
+		materials_hash.all?{|item,count|(owns_items[item]||0) >= count}
+	end
+	
+	def materials_to_s(**args)
+		Item.count_by_items_hash_to_s(materials_hash, **args)
+	end
+	def products_to_s(**args)
+		Item.count_by_items_hash_to_s(products_hash, **args)
+	end
+end
+
+RecipeAndCount = Struct.new(:recipe, :count, :group) do
+	def need_items
+		recipe.materials_hash.transform_values{|c|c*count}
+	end
+	def products_times_count
+		recipe.products_hash.transform_values{|c|c*count}
+	end
+	def craft_time
+		(recipe.production_time * count) / Math.log(group.soldier, 3) / (Math.log(count, 7) + 1)
+	end
+	
+	def materials_to_s(**args)
+		Item.count_by_items_hash_to_s(recipe.materials_hash.transform_values{|c|c*count}, **args)
+	end
+	def products_to_s(**args)
+		Item.count_by_items_hash_to_s(recipe.products_hash.transform_values{|c|c*count}, **args)
+	end
+end
